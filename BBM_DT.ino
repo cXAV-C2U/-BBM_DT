12fbd53ac477c34e36b984402902cc99b2522bdb
cXAV-C2U <starcodeung@gmail.com>
2024-09-15 22:20:05 +0100

    Create BBM_DT.ino

---
+++ BBM_DT.ino
@@ -0,0 +1,330 @@
+
+#BBM
+#Extreup
+  
+    TiTre "Dictionnaire"
+Dict.Form{Exp}{
+Detected [+] = Arp 
+Detected [-] = Guarp 
+     proud reload -CpU 
+      %p = 0; 
+           In p0 = Internal{
+   Expression (Load_Arp = Guarp){
+
+In delta.ARP = Exp.Set[+] 
+        
+        0 +Arp 
+        0 +Guarp
+       
+Load proud = Exp{Int}{
+
+Fast reload %p to proudLoad Arp: 
+Fast load %0 in proudLoad is Activated: 
+      
+     Dic [+] = %? In ProudParce is: 
+     #teutvaideur[+] = ProudParce[%?] 
+         
+       CpU is teutvaideur ProudParce[%] 
+SystemUpload CpU is Detected : 
+
+Dec[Exp].ProudLoad[%] 
+Dec[Exp].ProudLoad[+] 
+Dec[Exp].ProudLoad[-] 
+Dex :
+              %? : 
+       [0 -Guarp] 
+       [0 %: Exp] 
+       [.depeunding %0Guarp]
+    In 0 = %{*number}{
+    Execution #PKg[p0X][
+       #pixelTiTRe.install[Arp].Exec:
+       Pixel_Arp Reload 0%:
+     Axebelity Pixel_Arp = 0xP #dettails:
+         Reload%0x=Form.interactive{
+          PixelForm = 7.164
+          ProudLoad Form(-Pixel){
+     Determinate[p0X] = [+] : 
+       1 Determinate 1 : 
+         ProudLoad [ 0 ] 
+               p0X = PixelMega.Form: 
+       Determination 0xP is Load 1 : 
+        After 1 Determinate 1 : 
+          1 is [ Guarp ] 
+          1 is [ Arp ] 
+          Guarp determinate Arp 
+          Arp determinate Guarp 
+   Get PixelMega.Form To Speasialiseur the Guarp To : 
+     [ Arp ]  is  “ “+” , “-“ “ : 
+           Dettails proudeun[Determinate] 
+Det[0xP-PixelMega] : 
+    
+    proudeun PixelMega[ Parce ]
+  TrigaTeud[-] 
+  Guarp[+] 
+o By 0 
+0-0 . 2 = 1Mb 
+0mb -ExtruP 0I : 0Ex
+    0XE =SHare.Status -0.By 
+    0="?'TPP' -0
+    End
+         Status: 
+  Parce -0 = "?" 
+           StatuS 0By .Set -Parce -Ec
+  0 mb = 0.002 
+  1 mb = 0.007 
+      AlP=0.'x' 
+       dtt "?" -Ec 
+     Status 0 mb -Parce 
+     EltrUp -Bpd 0 by 0 
+      dec=mb +0I 
+     xml=mb +0I 
+      FasT Sersion -Bpd 
+      GguarP 0 interactive set : 
+          Ex -Extrup Parc 
+         by = 0-a [md]
+      --0a mb ==> interactive : 
+         set [0I-Md]
+        :!Status: 
+         dynamic Parce -get 
+         0mb < interactive: 
+             $Bpd =0 -a
+            +Bpd +a =Statusmb -FfTp\S
+    0==>FT:==0Ps -ExyruP 
+       {/0:-Ex.y} +i 
+            :Status:!
+        i=pB -Adreaa * Gguarp : 
+            dec \|0mb SeunsEur :!/bridge/
+        PixelMega*] +{-0<0} -StatusMb
+        FfTp\S -StatUsGp <Dtf
+        pArc 0mb==aO  ,1mb==a0 :
+            mTg $a0==Statusmb$  -Pixel
+      <dtc by 0=[%+i 
+      deHupS -P:0 actTivUt [by -0]
+          XMO={0p +Pixel==Up %AlP
+          Bt=+I -0! 
+          dtt=0 -End
+            Status $mb 
+             pProot mdb -by 0 
+        i0 Seun T- Guarp 
+             T=Pixel[-IdpP0Z 
+              RpD 0mb=>+p1
+              RpD 0mb=>+p0
+              RpD 0mb=>-0
+              RpD 0mb=>+0 
+             ResuLt -[MC a-RPdM]
+            RpDm -Mcc r-CD 
+        cO d- a0Mb 
+              *!uC -Is ReFuSe 
+             -FCU - [InTer1AcTive
+            ExP -PerssIon 
+            Cd Root By -mb 
+              AlP =~<% DTt: 
+                Sent%Root =+ 0%
+         %p0 -Root [Dex -ReCord0
+         Rp0 Sent !Pixel -n : 
+            GuarpMb Numeric|=0 
+         Exec GuarpMb :#p0Record
+         Final: 
+             GraveShap Cd.RPdM [FrOp]
+            p}DC IwP].Sent 
+        InTer1AcTive -0-ArP
+       // --0-- p1Record --0-- /:
+           Dettails-SecruP -Health 
+          ageyBey -Cd .?GrV 
+          [SeRssIon -Ar0]
+          Yevt -wp-2W
+                un6-Und -2Eu#%
+        Mb=ELuP .Gv INs 0* 
+        YebT -0Xt:ArL
+             SecRUp Gv -"#%"
+            1-CD "-0" -#
+        ArP LeDP -0Px  
+        0 StatUs ,#%SeRssIon
+          CaTng -p0 CT -r 
+            {0=gRaVe -SecryP
+            Und P0a-U2 :-0 
+            :0 --> U7 INs: 
+            :1/#ScP -p0iG 
+            :2/ScP -p0iG
+            :3/#ScP -p0iG
+            :4/#ScP -p0iG
+            :5/#ScP -p0iG
+           <:2/ScP -p0iG "!"
+           DHcP -Q p:196 
+           Sent .DHcP as "0IVC"
+          ExE -Exe 0-0 
+          EXwu-+] 72.1# 
+          wiWb-%0 PinG #rResnt/
+            gRaVe rResnr- 
+            --ENsEuP-- UpWn -a
+            -{'[q1'' -LomHPinH 
+            -ExP'0 ,Pp0==Mb
+            DHCP -PinG .GrV-00 
+            #ArTiC .GrV-01 
+            Fracp=uW#%-01 
+            Fracp=uW#%GrV-02
+            Fracp=uW#%-02  
+            Fracp=uW#%GrV-02
+            Fracp=uW#%-03
+            Fracp=uW#%GrC-02
+            #ArTiC .GrV-02 
+            -ExP0 ,P'p0==Mb
+            #ArTiC .GrV-01
+            Fracp=uW#%-04
+            Fracp=uW#%GrV-01
+            Fracp=uW#%-05
+            Fracp=uW#%GrV-01
+            Fracp=uW#%-06
+            Fracp=uW#%GrV-01
+            -ExP0 ,P'p0==MB
+            Fracp=uW#%-07
+            Fracp=uW#%GrV-07
+            Fracp=uW#%-08
+            Fracp=uW#%GrV-08
+            Fracp=uW#%-09
+            Fracp=uW#%GrV-09
+            -ExP0 ,P'p0==MB
+            Fracp=uW#%-01
+            Fracp=uW#%GrV-10
+            Fracp=uW#%-01
+            Fracp=uW#%GrV-11
+            Fracp=uW#%-01
+            Fracp=uW#%GrV-12
+            -ExP0 ,P'p0==MB
+            ['q1'' -LomPHpinH}
+           :2/ScP -p0iG "!">
+        0=gRaVe -Secryp}
+            -2Eu#% 
+       // --0-- p2Record --0-- /:      
+         Dex +ReCord=] -DHcP
+         DTC*^{GuaRpMb/#P9{
+         MTC-DeC -0 sent : 
+             SecryP -0:
+        SeNt Cd-Gr :?GrV-12
+           #SecryP>
+         Tab +EP:
+             1=~0=~1
+             1=~0=~0
+             0=~0=~1
+         Tab -EP:
+           <#SecryP 
+         Tab -EP:
+             1=~0=~0
+             0=~0=~1
+             1=~0=~0
+         Tab -EP:
+           #SecryP>
+         Tab -EP:
+             1=~0=~1
+             0=~0=~1
+             1=~1=~0
+         Tab -EP:
+           <#SecryP
+         Tab -EP:
+             1=~0=~0
+             0=~1=~1
+             1=~0=~1
+         Tab -EP:
+           #Secryp>
+           --DeUlp--
+           AxEunT==0: 
+             AxEunT==1:
+           AxEunT==2:
+             AxEunT==1:
+           AxEunT==2: 
+             AxEunT==1:
+           AxEunT==3:
+             AxEunT==1:
+           AxEunT==3:
+             AxEunT==1:
+           AxEunT==4:
+             AxEunT==1:
+           AxEunT==4:
+             AxEunT==1:
+           --DeUlp--
+         }
+          <-#SeCyP +1 : 
+          DeUsCP# %0-0 =1 
+          0*0=1 , 0+0=0.p0[#4]
+          -#4:0@p0
+          0*0=2 , 0+0=0.p1[#4]
+          1#=> -0i
+          0*0=3 , 0+0=0.p2[#4
+          1#=> -0i
+          0*0=4 , 0+0=0.p3[#4]
+           %0-0 =1 DeUsCP#
+          0*0=1 , 0+0=0.p0[#4]
+          -#4:0@p0
+          0*0=2 , 0+0=0.p1[#4]
+          1#=> -0i
+          0*0=3 , 0+0=0.p2[#4]
+          1#=> -0i
+          0*0=4 , 0+0=0.p3[#4]
+          DeUsCP# %0-0 =1 
+          0*0=1 , 0+0=0.p0[#4]
+          -#4:0@p0
+          0*0=2 , 0+0=0.p1[#4]
+          1#=> -0i
+          0*0=3 , 0+0=0.p2[#4
+          1#=> -0i
+          0*0=4 , 0+0=0.p3[#4]
+           %0-0 =1 DeUsCP#
+          0*0=1 , 0+0=0.p0[#4]
+          -#4:0@p0
+          0*0=2 , 0+0=0.p1[#4]
+          1#=> -0i
+          0*0=3 , 0+0=0.p2[#4]
+          1#=> -0i
+          0*0=4 , 0+0=0.p3[#4]
+          #SeCyP +1:>
+          InUe=GV -0:p3/0]
+          mTtGP:-0.0mb 
+          %-#00m -[p0mb 0:-md]
+              </:mTt- Determinate
+         < 0%-mTt#== 1 :,> #1%-mTt#
+         aplOid-dettails=p0mb.ex 
+         < 0%-mTt#== 0 :,> #2%-mTt#
+         aplOid-dettails=p0mb
+         < 0%-mTt#== 1 :,> #3%-mTt#
+         aplOid-dettails=p0mb
+         < 0%-mTt#== 1 :,> #4%-mTt#
+         apl0id-dettails=p0mb
+         pG/:~>0 == i %% pG/:~>0 >= i>
+         DtC-0:==>#DHcP]
+         <pG/:~>1 == i %% pG/:~>0 >= i 
+         
+          DTc-p#0*MegaParce>
+          ExtReuP-pG==0: 
+          
+         
+              
+               Determinate :tTm+/>
+          
+              
+          
+         
+          
+          
+         
+        
+            
+            
+        
+        
+          
+        
+            
+
+}
+
+}
+
+]
+}
+
+
+}
+
+}
+}
+}
